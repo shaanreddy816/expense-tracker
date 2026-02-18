9849f9ca9b890caf07c90c070b02a4c5216ec368
shaanreddy816 <shanthanreddy5000@gmail.com>
2026-02-18 08:39:53 -0500

    Update redirect URIs for Vercel

--- src/authConfig.js
+++ src/authConfig.js
@@ -8,8 +8,8 @@ export const authConfig = {
   // Use the IDP endpoint as the authority – it returns the correct discovery document
   authority: "https://cognito-idp.us-east-2.amazonaws.com/us-east-2_p9qfpUExB",
   client_id: "hefqfkkhb8u83t55uva6328o1",
-  redirect_uri: "http://localhost:5173/dashboard",
-  post_logout_redirect_uri: "http://localhost:5173/",
+  redirect_uri: "https://expense-tracker.vercel.app/dashboard",
+  post_logout_redirect_uri: "https://expense-tracker.vercel.app/",
   response_type: "code",
   scope: "openid email profile",
   userStore: new WebStorageStateStore({ store: window.localStorage }),
